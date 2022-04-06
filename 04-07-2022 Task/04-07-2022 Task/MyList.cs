6853b0f449900612f8d026fd7102c8f0c53023a5
SevdaKhidirova <70jrjy2@ode.edu.az>
2022-04-07 01:56:10 +0400

    task

---
+++ 04-07-2022 Task/04-07-2022 Task/MyList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_04_2022_Task
+{
+    class MyList<T>
+    {
+        private T[] _arr = new T[0];
+        private int _count;
+        private int capacity = 0;
+        private int length = 0;
+
+
+        public void AddItem(T number)
+        {
+            if (ResizeAble())
+                if (capacity == 0)
+                {
+                    capacity += 1;
+                }
+                else
+                {
+                    capacity *= 2;
+                }
+            Array.Resize(ref _arr, capacity);
+
+            _arr[length] = number;
+            length++;
+
+        }
+        public void AddItems(params T[] numbers)
+        {
+            foreach (var item in numbers)
+            {
+                AddItem(item);
+            }
+        }
+        private bool ResizeAble()
+        {
+            if (length == capacity)
+                return true;
+            return false;
+        }
+        public void Capacity()
+        {
+            Console.WriteLine(capacity);
+        }
+        public void Count()
+        {
+            _count = length;
+            Console.WriteLine(_count);
+        }
+
+        public void Clear()
+        {
+            _arr = new T[0];
+            length = 0;
+            _count = 0;
+            capacity = 0;
+        }
+
+        public bool Exsist(T number)
+        {
+            foreach (T item in _arr)
+            {
+                if (number.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public void PrintArrayElements()
+        {
+            foreach (var item in _arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Reverse()
+        {
+           for(int i = length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+        public void Indexof(T number)
+        {
+            for(int i = 0; i < length; i++)
+            {
+                if (number.Equals(_arr[i]))
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("not foud");
+        }
+        public void LastIndexOf(T number)
+        {
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (number.Equals(_arr[i]))
+                {
+                    Console.WriteLine(_arr[i]);
+                    return;
+                }
+            }
+            Console.WriteLine("not found");
+        }
+
+    }
+}
---
+++ 04-07-2022 Task/04-07-2022 Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _07_04_2022_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> ls = new MyList<int>();
+            ls.AddItem(5);
+            ls.AddItem(8);
+            ls.AddItem(9);
+            ls.AddItems(3, 4, 6,6,8);
+
+            //ls.PrintArrayElements();
+
+            // Console.WriteLine(ls.Exsist(5) ); 
+            //ls.Reverse();
+            ls.Indexof(0);
+            ls.LastIndexOf(6);
+
+        }
+    }
+}
+/*C#-da istifadə etdiyimiz. List<T> classını custom olaraq yazın.
+Indexlə get və ya set etmək mümkün olsun.
+- Capacity
+- Count
+- Add()
+- Clear()
+- Exist()
+- Remove()
+- Reverse()
+- IndexOf()
+- LastIndexOf()
+memberləri olacaq*/
